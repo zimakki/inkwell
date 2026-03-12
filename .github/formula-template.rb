cbb44112b7ec5b420d28d8baf75304ca49d09dc6
Zi Makki <zimakki@gmail.com>
2026-03-12 19:47:27 +0100

    Update Homebrew formula for Burrito platform-specific binaries

--- .github/formula-template.rb
+++ .github/formula-template.rb
@@ -1,15 +1,29 @@
 class Inkwell < Formula
   desc "Live markdown preview daemon with file picker and fuzzy search"
   homepage "https://github.com/zimakki/inkwell"
-  url "https://github.com/zimakki/inkwell/releases/download/v__VERSION__/inkwell"
-  version "__VERSION__"
-  sha256 "__SHA256__"
   license "MIT"
+  version "__VERSION__"
 
-  depends_on "erlang"
+  on_macos do
+    on_arm do
+      url "https://github.com/zimakki/inkwell/releases/download/v__VERSION__/inkwell_darwin_arm64"
+      sha256 "__SHA256_DARWIN_ARM64__"
+    end
+    on_intel do
+      url "https://github.com/zimakki/inkwell/releases/download/v__VERSION__/inkwell_darwin_amd64"
+      sha256 "__SHA256_DARWIN_AMD64__"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/zimakki/inkwell/releases/download/v__VERSION__/inkwell_linux_amd64"
+      sha256 "__SHA256_LINUX_AMD64__"
+    end
+  end
 
   def install
-    bin.install "inkwell"
+    bin.install Dir.glob("inkwell*").first => "inkwell"
   end
 
   test do
