40c7417113b603d7f8fb8385a238cfcc17c905a3
Zi Makki <zimakki@gmail.com>
2026-03-31 05:08:49 +0200

    Bump version to 0.2.21

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Inkwell.MixProject do
   def project do
     [
       app: :inkwell,
-      version: "0.2.20",
+      version: "0.2.21",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
